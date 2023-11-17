2c2f9378740c5f6cfc39e3723940390182beff87
moust1que <emiliensalpeteur@gmail.com>
2023-11-17 22:43:51 +0100

    EMILIEN_PlayerMove
    - Add player move horizontally
    - Add player jump

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,18 +1,39 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Windows;
 
-public class Player : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+[RequireComponent(typeof(CharacterController))]
+public class Player : MonoBehaviour {
+    [SerializeField] private float _speedX = 10.0f;
+    [SerializeField] private float _jumpHeight = 1.0f;
+    [SerializeField] private float _speedY = 0.25f;
+    private const float _gravity = -9.81f;
+
+    void Update() {
+        CharacterController controller = GetComponent<CharacterController>();
+
+        Vector3 forward = transform.TransformDirection(Vector3.right);
+        float curSpeed = _speedX * UnityEngine.Input.GetAxis("Horizontal");
+        controller.SimpleMove(forward * curSpeed);
+
+        Vector3 jump = transform.TransformDirection(Vector3.up);
+
+        if(IsGrounded() && UnityEngine.Input.GetKey(KeyCode.Space)) {
+            Vector3 movementVelocity = new Vector3();
+            movementVelocity.y = Mathf.Sqrt(_speedY * _jumpHeight * _gravity * -1.0f);
+            movementVelocity.y *= 1.0f;
+            gameObject.transform.position = new Vector3(gameObject.transform.position.x, movementVelocity.y, gameObject.transform.position.z);
+        }else {
+
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private bool IsGrounded() {
+        if(gameObject.transform.position.y > 1.1f)
+            return false;
+
+        return true;
     }
 }
