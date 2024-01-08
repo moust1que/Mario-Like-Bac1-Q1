e996eb99c7e66be6f551adbc83defa992205410e
Jul-b1jv6 <julien.a.savoye@gmail.com>
2024-01-08 14:49:44 +0100

    julien_ennemipart1

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    bool timerActive = false;
+    float currentTime;
+    public int startMinutes;
+    public Text currentTimeText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTime = startMinutes*60;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timerActive == true)
+        {
+            currentTime = currentTime - Time.deltaTime;
+
+        }
+        currentTimeText.text = currentTime.ToString();
+    }
+
+   
+}
