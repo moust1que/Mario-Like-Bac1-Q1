bf2112ffa2ddb5ab10f836bed843939cdc1e8df4
Hildadia <marion.fusillier.mf@gmail.com>
2024-01-09 23:25:25 +0100

    marion_
    falldetection end

---
+++ Assets/Scripts/Falldetection.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Falldetection : MonoBehaviour
+{
+    public GameManager gameManager;
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.transform.position.y <= -2)
+        {
+            gameManager.DeathPlayer();
+        }
+    }
+}
