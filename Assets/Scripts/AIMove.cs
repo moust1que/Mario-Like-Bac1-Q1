6c04420524e71d36b47316421b6bbb02652184e5
moust1que <emiliensalpeteur@gmail.com>
2024-01-13 03:18:10 +0100

    EMILIEN_koopa
    Add Koopa's

--- Assets/Scripts/AIMove.cs
+++ Assets/Scripts/AIMove.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 public class AIMove : MonoBehaviour {
 	private Rigidbody m_rigidbody;
     [SerializeField] private float m_moveSpeed = 15.0f;
-	private bool m_direction = true;
+	[SerializeField] private bool m_direction = true;
 
 	private void Start() {
 		m_rigidbody = GetComponent<Rigidbody>();
@@ -16,11 +16,13 @@ public class AIMove : MonoBehaviour {
 
 	private void HandleHorizontalMovement() {
 		transform.Translate(Vector3.right * (m_direction ? m_moveSpeed : -1.0f * m_moveSpeed) * Time.deltaTime);
-		if(Physics.Raycast(m_rigidbody.position, m_direction == true ? Vector3.right : Vector3.left, gameObject.transform.localScale.x / 2.0f)) {
+		if(Physics.Raycast(m_rigidbody.position, m_direction == true ? Vector3.right : Vector3.left, gameObject.transform.localScale.x / 2.0f + 0.1f)) {
 			if(m_direction)
 				m_direction = false;
 			else
 				m_direction = true;
+			
+			transform.GetChild(0).transform.Rotate(new Vector3(0.0f, 180.0f, 0.0f));
 		}
 	}
 }
